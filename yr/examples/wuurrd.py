f03db41738326946cad8b9c95db0a438769575ed
Øystein Bjørndal <obtitus@gmail.com>
2021-12-27 19:04:38 +0100

    not 100% compatible, seems the responce now has a mix of fields for different time entries.

--- yr/examples/wuurrd.py
+++ yr/examples/wuurrd.py
@@ -4,8 +4,15 @@ from yr.libyr import Yr
 
 weather = Yr(location_name='Norge/Telemark/Skien/Skien', language_name='nb')
 
+data = list()
+for forecast in weather.forecast():
+    if 'windSpeed' in forecast:
+        row = {'from': forecast['@from'], 'to': forecast['@to'], 'speed': float(forecast['windSpeed']['@mps'])}
+        data.append(row)
+
 wind_speed = dict()
-wind_speed['data'] = [{'from': forecast['@from'], 'to': forecast['@to'], 'speed': float(forecast['windSpeed']['@mps'])} for forecast in weather.forecast()]
-wind_speed['credit'] = weather.credit
+wind_speed['data'] = data
+wind_speed['credit'] = weather.credit        
+
 
 print(wind_speed)
