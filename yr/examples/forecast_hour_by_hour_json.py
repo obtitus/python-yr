9547026619c5f41e72b3c2e69e5ff5ee27edc4ae
Øystein Bjørndal <obtitus@gmail.com>
2021-12-27 19:21:10 +0100

    added fixme

--- yr/examples/forecast_hour_by_hour_json.py
+++ yr/examples/forecast_hour_by_hour_json.py
@@ -7,6 +7,7 @@ weather = Yr(location_name='Norway/Rogaland/Stavanger/Stavanger')
 
 hour = datetime.timedelta(hours=1)
 for forecast_json in weather.forecast(as_json=True):
+    # FIXME: not very user-friendly, add this filtering to the yr library?
     forecast = json.loads(forecast_json)
     time_from = datetime.datetime.strptime(forecast['@from'], '%Y-%m-%dT%H:%M:%SZ')
     time_to   = datetime.datetime.strptime(forecast['@to'], '%Y-%m-%dT%H:%M:%SZ')
