0626a5d39baf6c4f48bdba6a93354c4b1b895caa
Øystein Bjørndal <obtitus@gmail.com>
2021-12-27 19:18:49 +0100

    modified hour by hour example, 2.0 API returns all entries, parse and only print relevant entries.

--- yr/examples/forecast_hour_by_hour_json.py
+++ yr/examples/forecast_hour_by_hour_json.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-
+import datetime
+import json
 from yr.libyr import Yr
 
-weather = Yr(location_name='Norway/Rogaland/Stavanger/Stavanger',
-             forecast_link='forecast_hour_by_hour',
-             language_name='nb')
+weather = Yr(location_name='Norway/Rogaland/Stavanger/Stavanger')
 
-for forecast in weather.forecast(as_json=True):
-    print(forecast)
+hour = datetime.timedelta(hours=1)
+for forecast_json in weather.forecast(as_json=True):
+    forecast = json.loads(forecast_json)
+    time_from = datetime.datetime.strptime(forecast['@from'], '%Y-%m-%dT%H:%M:%SZ')
+    time_to   = datetime.datetime.strptime(forecast['@to'], '%Y-%m-%dT%H:%M:%SZ')
+    delta = time_to - time_from
+    
+    if delta == hour: # hour_by_hour only
+        print(forecast_json)
+
