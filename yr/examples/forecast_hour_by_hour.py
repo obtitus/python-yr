83315e63f9f0d171ae46c88ef31ba15497faf64c
Øystein Bjørndal <obtitus@gmail.com>
2021-12-27 19:15:30 +0100

    modified hour by hour example, 2.0 API returns all entries, parse and only print relevant entries.

--- yr/examples/forecast_hour_by_hour.py
+++ yr/examples/forecast_hour_by_hour.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
-
+import datetime
 from yr.libyr import Yr
 
-weather = Yr(location_name='Norway/Rogaland/Stavanger/Stavanger',
-             forecast_link='forecast_hour_by_hour')
+weather = Yr(location_name='Norway/Rogaland/Stavanger/Stavanger')
 
+hour = datetime.timedelta(hours=1)
 for forecast in weather.forecast():
-    print(forecast)
+    # ('@from', '2022-01-05T18:00:00Z'), ('@to', '2022-01-05T18:00:00Z')
+    time_from = datetime.datetime.strptime(forecast['@from'], '%Y-%m-%dT%H:%M:%SZ')
+    time_to   = datetime.datetime.strptime(forecast['@to'], '%Y-%m-%dT%H:%M:%SZ')
+    delta = time_to - time_from
+    
+    if delta == hour: # hour_by_hour only
+        print(forecast)
